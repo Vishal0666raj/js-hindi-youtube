38ae722eb8321ffd0f40cf59217baf4ba29d4ae9
Vishal0666raj <jimmy91648@gmail.com>
2025-03-06 23:52:38 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as new version
+
+//alert(3+3) we are using nodejs not browser
+
+console.log(3
+    +
+    3); //code readability should be high
+
+console.log("Vishal");
+
+let name = "Vishal"
+let age = 21
+let isLoggedIn = true
+
+//number =>2 to poewr 53
+//bigint
+//boolean
+//string = ""
+//null => standalone
+//undefined 
+//symbol => unique 
+
+//object
+
+console.log(typeof "Vishal");
+console.log(typeof null); //=> object
+console.log(typeof undefined); //=> undefined
+
+
+    
