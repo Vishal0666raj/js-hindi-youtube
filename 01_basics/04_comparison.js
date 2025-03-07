7344f14c6b0c4b35ef35a947d7b6b24040433e70
Vishal0666raj <jimmy91648@gmail.com>
2025-03-07 01:40:30 +0000

    container config done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,42 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+//inme koi dikkat nhiu hai (they are best)
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+//on comapring bw diff datatypes it will change into same and compare
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// here the equality and comaprison operator works differently 
+
+
+// console.log(undefined ==0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+//always false 
+
+/// strict cheking "===" here it will not only check the data but the data type as well
+// console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// Primitve 
+
+// 7 types : string , Number , Boolean , null , undefined , symbol , Bigint 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+// console.log(id === anotherId);
+
+const bigNumber = 38478734864864876476n;
+
+//Reference (non - primtive)
+
+// Array , Object and Functions
+
+const heros = ["shaktiman","naagraj","doga"];
+let myObj = {
+    name:"Vishal",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof id);
