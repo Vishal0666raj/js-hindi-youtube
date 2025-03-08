4fa15892a043f72f59593bfd0cf90891639bb6de
Vishal0666raj <jimmy91648@gmail.com>
2025-03-08 06:26:50 +0000

    container config done

---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,56 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100.23456);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const anotherNumber = 123.8765
+// console.log(anotherNumber.toPrecision(5));
+// console.log(anotherNumber.toPrecision(4));
+// console.log(anotherNumber.toPrecision(3));
+// console.log(anotherNumber.toPrecision(2));
+
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString());
+
+
+
+//+++++++++Maths++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.4));
+// console.log(Math.ceil(5.01));
+// console.log(Math.floor(5.99));
+// console.log(Math.min(1,2,3,4,5,6));
+// console.log(Math.max(1,2,3,4,5,6,7,8));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10)+1);
+// const min = 10
+// const max = 20
+// console.log(Math.floor(Math.random()*(max - min +1)) + min);
+
+const dice_min = 1
+const dice_max = 6 
+console.log(Math.floor(Math.random()*(dice_max - dice_min +1)) + dice_min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
