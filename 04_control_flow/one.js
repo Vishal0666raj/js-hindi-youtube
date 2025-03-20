fcca79f6864303356d708b3da978c4de1d588901
Vishal0666raj <jimmy91648@gmail.com>
2025-03-20 02:28:36 +0000

    container config done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,72 @@
+//if
+const isUserloggedIn = true
+
+// if( 2!== "2" ){
+//     console.log("Executed");
+// }
+
+// > , < , <= , >= , == , === , != , !==
+
+const temperature = 41
+// if(temperature === 41){
+//      console.log("tmperature is less than 50");
+     
+// }
+// else{
+//      console.log("Termoparture is grater than 50");
+     
+// }
+
+
+const score = 200 
+// if(score === 200){
+//     const power = "Fly";
+//     console.log(`Power is : ${power}`);
+    
+// }
+// console.log(`Power is : ${power}`);
+
+
+
+//short hand notation 
+const balance = 1000
+
+// if(balance > 500) console.log("Test1") , console.log("Test2");
+
+
+
+
+// if(balance < 500){
+//     console.log("balance is less than 500");
+    
+// }
+// else if(balance < 750){
+//     console.log("Balance is less than 750");
+    
+// }
+// else{
+//     console.log("Balance is less than 1200");
+    
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+
+// if(userLoggedIn && debitCard){
+//     console.log("User are allowed to shop.");
+    
+// }
+
+const loggedInFromGoogle = true
+const loggedInFromEmail = false 
+
+// if(loggedInFromEmail || loggedInFromGoogle){
+//     console.log("user is Logged");
+    
+// }
+
+
+
+
+
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,63 @@
+const email = "Vishal91648@gmail.com";
+
+// if(email){
+//     console.log("Got the email.");
+    
+// }
+// else{
+//     console.log("Did not got the email.");
+    
+// }
+
+
+//falsy 
+// 0 , false , "" , -0 , BigInt 0n , null , undefined , NaN
+
+//truthy
+// "0" , " " , 'false' , [] , {} , function(){}
+
+const arr = []
+// if(arr){
+//     console.log("Empty array");
+    
+// }  not the right way to check
+
+// if(arr.length === 0){
+//     console.log("Array is empty");
+    
+// }
+
+
+const obj = {};
+
+// if(Object.keys(obj).length === 0 ){
+//     console.log("Object is empty.");
+    
+// }
+
+
+//Nullish Coalescing Operator (??): null undefined 
+
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 20
+// val1 = null ?? 10 ?? 20
+
+
+
+
+// console.log(val1);
+
+
+
+
+//Ternary operator
+// condition ? true : false ;
+
+const iceTeaPrice = 100
+// iceTeaPrice <= 80 ? console.log("Price is lesser than 80") : console.log("Price is greater than 80.");
+
+
+
+
