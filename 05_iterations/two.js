8fcb7830b8edad1e8e4a656443de810dfd9c9912
Vishal0666raj <jimmy91648@gmail.com>
2025-03-22 00:27:09 +0000

    container config done

---
+++ 05_iterations/two.js
@@ -0,0 +1,30 @@
+// while 
+
+let index = 0;
+while (index <= 10) {
+    // console.log(`The value of index is : ${index}`);
+    index = index + 2;
+}
+
+
+let myarray = ["Iron Man" , "BatMan" , " Flash"]
+
+let arr = 0
+
+while (arr <  myarray.length) {
+    // console.log(`The name of the superHeros are : ${myarray[arr]}`);
+    arr = arr + 1
+    
+}
+
+
+
+// do while 
+
+let score = 11
+
+do {
+    // console.log(`The score is ${score}`);
+    score++;
+    
+} while (score <= 10);
