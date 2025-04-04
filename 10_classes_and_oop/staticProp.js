b58e7c627af267b4f55c2221f9c42083e14b76fd
Vishal0666raj <jimmy91648@gmail.com>
2025-04-04 01:11:19 +0000

    container config done

---
+++ 10_classes_and_oop/staticProp.js
@@ -0,0 +1,34 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return Math.round(Math.random()*100+1);
+    }
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
+
+
+
+// static methods belong to the class, not the instance.
+
+// You cannot access a static method with an object (object.staticMethod()).
+
+// You can access it via the class (ClassName.staticMethod()).
